6707c4aa7c1aeb12e65acd3dbb0c10f54caeafc7
rpresende <rpresende@stefanini.com>
2024-03-08 16:56:34 -0300

    change theme implemented

--- src/app/modules/shared/components/header/header.component.ts
+++ src/app/modules/shared/components/header/header.component.ts
@@ -10,7 +10,23 @@ export class HeaderComponent implements OnInit {
 
   constructor(protected router: Router) { }
 
+  isDark = true 
+
   ngOnInit(): void {
+
+    const storedIsDark = localStorage.getItem('isDark');
+
+    if (storedIsDark !== null) {
+      this.isDark = JSON.parse(storedIsDark);
+    }
+    
+    document.body.className = this.isDark ? 'dark' : 'light'
+  }
+
+  toggleTheme(){
+    this.isDark = !this.isDark
+    localStorage.setItem('isDark', JSON.stringify(this.isDark));
+    document.body.className = this.isDark ? 'dark' : 'light'
   }
 
 }
